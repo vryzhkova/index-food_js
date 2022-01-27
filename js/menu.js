f3c2113f6b60be831894d240d3dfeb1e98152e9d
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-01-27 15:56:34 +0300

    added render items

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,57 @@
-const restourant = 'tanuki';
+const cardsMenu = document.querySelector('.cards-menu');
 
-const renderItems = (data) => {
-    console.log(data);
+const changeTitle = (restaurant) => {
+    const restaurantTitle = document.querySelector('.restaurant-title');
+
+    restaurantTitle.textContent = restaurant.name;
 }
 
-fetch(`./db/${restourant}.json`)
-    .then((response) => response.json())
-    .then((data) => {
-        renderItems(data)
+const renderItems = (data) => {
+    data.forEach(({ description, id, image, name, price }) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <img src="${image}" alt="${image}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="ingredients">
+                    ${description}
+                    </div>
+                </div>
+                <!-- /.card-info -->
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price}</strong>
+                </div>
+            </div>
+        `
+
+        cardsMenu.append(card);
     })
-    .catch((error) => {
-        console.log(error)
-    })
+}
+
+if(localStorage.getItem('restaurant')) {
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'));
+
+    changeTitle(restaurant);
+
+        fetch(`./db/${restaurant.products}`)
+            .then((response) => response.json())
+            .then((data) => {
+                renderItems(data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+} else {
+    window.location.href = '/';
+}
+
--- js/partners.js
+++ js/partners.js
@@ -1,5 +1,43 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+
 const renderItems = (data) => {
-    console.log(data);
+    data.forEach((item) => {
+        const { image, kitchen, name, price, products, stars, time_of_delivery } = item;
+        const a = document.createElement('a');
+
+        a.setAttribute('href', '/restaurant.html');
+        a.classList.add('card');
+        a.classList.add('card-restaurant');
+        a.dataset.products = products;
+
+        a.innerHTML = `
+                <img src="${image}" alt="${image}" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title">${name}</h3>
+                        <span class="card-tag tag">${time_of_delivery} мин</span>
+                    </div>
+                    <!-- /.card-heading -->
+                    <div class="card-info">
+                        <div class="rating">
+                        ${stars}
+                        </div>
+                        <div class="price">${price}</div>
+                        <div class="category">${kitchen}</div>
+                    </div>
+                </div>
+            `
+
+            a.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                localStorage.setItem('restaurant', JSON.stringify(item));
+
+                window.location.href = '/restaurant.html';
+            })
+
+            cardsRestaurants.append(a);
+    })
 }
 
 fetch('https://test-2-8c729-default-rtdb.firebaseio.com/db/partners.json')
