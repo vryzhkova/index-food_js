523baed7026d57cdfad9fe3e4bff7c1fe91b0b53
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-01-30 16:42:05 +0300

    added sending data to the server from the shopping cart

--- js/auth.js
+++ js/auth.js
@@ -1,56 +1,63 @@
-const buttonAuth = document.querySelector('.button-auth');
-const modalAuth = document.querySelector('.modal-auth');
-const closeAuth = document.querySelector('.close-auth');
-const buttonOut = document.querySelector('.button-out');
-const userName = document.querySelector('.user-name');
-const logInForm = document.getElementById('logInForm');
-const inputLogin = document.getElementById('login');
-const inputPassword = document.getElementById('password');
+const auth = () => {
+    const buttonAuth = document.querySelector('.button-auth');
+    const modalAuth = document.querySelector('.modal-auth');
+    const closeAuth = document.querySelector('.close-auth');
+    const buttonOut = document.querySelector('.button-out');
+    const userName = document.querySelector('.user-name');
+    const logInForm = document.getElementById('logInForm');
+    const inputLogin = document.getElementById('login');
+    const inputPassword = document.getElementById('password');
+    const buttonCart = document.querySelector('.button-cart');
 
-const login = (user) => {
-    buttonAuth.style.display = 'none';
+    const login = (user) => {
+        buttonAuth.style.display = 'none';
 
-    buttonOut.style.display = 'flex';
-    userName.style.display = 'flex';
+        buttonOut.style.display = 'flex';
+        userName.style.display = 'flex';
+        buttonCart.style.display = 'flex';
 
-    userName.textContent = user.login;
-    modalAuth.style.display = 'none';
-}
-
-const logout = () => {
-    buttonAuth.style.display = 'flex';
-
-    buttonOut.style.display = 'none';
-    userName.style.display = 'none';
-    userName.textContent = '';
-
-    localStorage.removeItem('user')
-}
-
-buttonAuth.addEventListener('click', () => {
-    modalAuth.style.display = 'flex';
-})
-
-closeAuth.addEventListener('click', () => {
-    modalAuth.style.display = 'none';
-})
-
-buttonOut.addEventListener('click', () => {
-    logout();
-})
-
-logInForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const user = {
-        login: inputLogin.value,
-        passowrd: inputPassword.value,
+        userName.textContent = user.login;
+        modalAuth.style.display = 'none';
     }
 
-    localStorage.setItem('user', JSON.stringify(user));
-    login(user);
-})
+    const logout = () => {
+        buttonAuth.style.display = 'flex';
 
-if(localStorage.getItem('user')) {
-    login(JSON.parse(localStorage.getItem('user')));
+        buttonOut.style.display = 'none';
+        userName.style.display = 'none';
+        userName.textContent = '';
+        buttonCart.style.display = 'none';
+
+        localStorage.removeItem('user')
+    }
+
+    buttonAuth.addEventListener('click', () => {
+        modalAuth.style.display = 'flex';
+    })
+
+    closeAuth.addEventListener('click', () => {
+        modalAuth.style.display = 'none';
+    })
+
+    buttonOut.addEventListener('click', () => {
+        logout();
+    })
+
+    logInForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const user = {
+            login: inputLogin.value,
+            passowrd: inputPassword.value,
+        }
+
+        localStorage.setItem('user', JSON.stringify(user));
+        login(user);
+    })
+
+    if(localStorage.getItem('user')) {
+        login(JSON.parse(localStorage.getItem('user')));
+    }
 }
+
+auth();
--- js/partners.js
+++ js/partners.js
@@ -1,50 +1,54 @@
-const cardsRestaurants = document.querySelector('.cards-restaurants');
+const parthers = () => {
+    const cardsRestaurants = document.querySelector('.cards-restaurants');
 
-const renderItems = (data) => {
-    data.forEach((item) => {
-        const { image, kitchen, name, price, products, stars, time_of_delivery } = item;
-        const a = document.createElement('a');
+    const renderItems = (data) => {
+        data.forEach((item) => {
+            const { image, kitchen, name, price, products, stars, time_of_delivery } = item;
+            const a = document.createElement('a');
 
-        a.setAttribute('href', '/restaurant.html');
-        a.classList.add('card');
-        a.classList.add('card-restaurant');
-        a.dataset.products = products;
+            a.setAttribute('href', '/restaurant.html');
+            a.classList.add('card');
+            a.classList.add('card-restaurant');
+            a.dataset.products = products;
 
-        a.innerHTML = `
-                <img src="${image}" alt="${image}" class="card-image" />
-                <div class="card-text">
-                    <div class="card-heading">
-                        <h3 class="card-title">${name}</h3>
-                        <span class="card-tag tag">${time_of_delivery} мин</span>
-                    </div>
-                    <!-- /.card-heading -->
-                    <div class="card-info">
-                        <div class="rating">
-                        ${stars}
+            a.innerHTML = `
+                    <img src="${image}" alt="${image}" class="card-image" />
+                    <div class="card-text">
+                        <div class="card-heading">
+                            <h3 class="card-title">${name}</h3>
+                            <span class="card-tag tag">${time_of_delivery} мин</span>
                         </div>
-                        <div class="price">${price}</div>
-                        <div class="category">${kitchen}</div>
+                        <!-- /.card-heading -->
+                        <div class="card-info">
+                            <div class="rating">
+                            ${stars}
+                            </div>
+                            <div class="price">${price}</div>
+                            <div class="category">${kitchen}</div>
+                        </div>
                     </div>
-                </div>
-            `
+                `
 
-            a.addEventListener('click', (event) => {
-                event.preventDefault();
+                a.addEventListener('click', (event) => {
+                    event.preventDefault();
 
-                localStorage.setItem('restaurant', JSON.stringify(item));
+                    localStorage.setItem('restaurant', JSON.stringify(item));
 
-                window.location.href = '/restaurant.html';
-            })
+                    window.location.href = '/restaurant.html';
+                })
 
-            cardsRestaurants.append(a);
-    })
+                cardsRestaurants.append(a);
+        })
+    }
+
+    fetch('https://test-2-8c729-default-rtdb.firebaseio.com/db/partners.json')
+        .then((response) => response.json())
+        .then((data) => {
+            renderItems(data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 
-fetch('https://test-2-8c729-default-rtdb.firebaseio.com/db/partners.json')
-    .then((response) => response.json())
-    .then((data) => {
-        renderItems(data)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+parthers();
